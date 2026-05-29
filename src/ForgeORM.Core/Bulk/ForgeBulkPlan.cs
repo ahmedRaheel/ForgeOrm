e3e2ed0696c8892c726b7cf3fe3e9806e1abb991
ahmedRaheel <raaheelahmed@outlook.com>
2026-05-29 15:02:04 +0500

    corrected model

--- src/ForgeORM.Core/Bulk/ForgeBulkPlan.cs
+++ src/ForgeORM.Core/Bulk/ForgeBulkPlan.cs
@@ -1,22 +1,80 @@
+using Microsoft.Data.SqlClient.Server;
+using System.Data;
+using System.Reflection;
+
 namespace ForgeORM.Core;
 
 public sealed class ForgeBulkPlan
 {
-    public required string TableName { get; init; }
-    public required string QuotedTableName { get; init; }
+    private readonly Func<object, object?>[] _getters;
+    private readonly Type[] _declaredTypes;
+    private readonly DataTable _schema;
 
-    public required string InsertSql { get; init; }
-    public required string UpdateSql { get; init; }
-    public required string DeleteSql { get; init; }
+    private ForgeBulkPlan(
+        string quotedTableName,
+        string tvpTypeName,
+        string insertSql,
+        PropertyInfo[] properties,
+        string[] columnNames,
+        Func<object, object?>[] getters,
+        Type[] declaredTypes,
+        DataTable schema,
+        SqlMetaData[] sqlMetaData)
+    {
+        QuotedTableName = quotedTableName;
+        TvpTypeName = tvpTypeName;
+        InsertSql = insertSql;
+        Properties = properties;
+        ColumnNames = columnNames;
+        _getters = getters;
+        _declaredTypes = declaredTypes;
+        _schema = schema;
+        SqlMetaData = sqlMetaData;
+    }
 
-    public required string KeyColumn { get; init; }
-    public required Type KeyClrType { get; init; }
+    public string QuotedTableName { get; }
+    public string TvpTypeName { get; }
+    public string InsertSql { get; }
+    public SqlMetaData[] SqlMetaData { get; }
+    public PropertyInfo[] Properties { get; }
+    public string[] ColumnNames { get; }
 
-    public required IReadOnlyList<ForgeBulkColumn> Columns { get; init; }
+    public static ForgeBulkPlan Create(Type entityType, string tableName)
+    {
+        var properties = GetBulkProperties(entityType, includeIdentity: false);
+        var columnNames = BuildColumnNames(properties);
+        return new ForgeBulkPlan(
+            QuoteTable(tableName),
+            BuildTvpTypeName(entityType),
+            BuildInsertFromTvpSql(tableName, columnNames),
+            properties,
+            columnNames,
+            BuildGetters(properties),
+            BuildDeclaredTypes(properties),
+            CreateSchema(properties),
+            BuildSqlMetaData(properties));
+    }
 
-    public string? TvpTypeName { get; init; }
-    public string? KeyTvpTypeName { get; init; }
-    public string? TempTableName { get; init; }
-    public string? SchemaName { get; init; }
+    public object? GetValue(object entity, int ordinal) => _getters[ordinal](entity);
+    public Type GetDeclaredType(int ordinal) => _declaredTypes[ordinal];
+
+    public DataTable CreateTable<T>(IReadOnlyList<T> rows)
+    {
+        var table = _schema.Clone();
+        table.BeginLoadData();
+
+        for (var r = 0; r < rows.Count; r++)
+        {
+            var dataRow = table.NewRow();
+            var entity = rows[r]!;
+            for (var c = 0; c < _getters.Length; c++)
+                dataRow[c] = NormalizeValue(_getters[c](entity), _declaredTypes[c]) ?? DBNull.Value;
+            table.Rows.Add(dataRow);
+        }
+
+        table.EndLoadData();
+        return table;
+    }
+
 }
 
